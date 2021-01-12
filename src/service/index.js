fcce9af1b647b1fbefadf520309b23d91e1e7816
Larissa <larissa@fantasticocomic.com>
2021-01-12 16:36:35 -0600

    Add headers

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const headers = {
-  "Content-Type": "application/json",
+  "content-Type": "application/json",
+  accept: "application/json",
 };
 
 const baseURL = () => {
